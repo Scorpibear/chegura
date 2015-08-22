3131bf18026c2cf7dc2daa4df7cadf601383fa8f
Mikhail Subach <scorpibear@tut.by>
2015-08-22 18:55:47 +0300

    evaluation module extracted

---
+++ app/evaluation.js
@@ -0,0 +1,16 @@
+var evaluationLogFileName = 'evaluations.log'
+var baseManager = require('./base-manager')
+var fs = require('fs')
+
+module.exports.register = function(moveList, moveObject, relativeCentipawnScore, depth) {
+    var resultBestmove = moveObject.san
+	var scoreValue = relativeCentipawnScore / 100.0
+	if (moveObject.color == 'b')
+	    scoreValue = -scoreValue
+	console.log("best move for ", moveList, " is ", resultBestmove, " with score/depth ", scoreValue, "/", depth)
+	fs.appendFile(evaluationLogFileName, moveList.join(' ') + ' ' + resultBestmove + ' ' + scoreValue + '/' + depth, function (err) {
+		if (err) console.error("could not append to '"+evaluationLogFileName+"' :", err)
+	})
+	//if(!baseManager) throw Error('base manager is not defined. Call analyzer.setBaseManager before')
+	baseManager.addToBase(moveList, resultBestmove, scoreValue, depth)
+}
