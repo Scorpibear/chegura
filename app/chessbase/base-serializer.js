12f8004fba5af386c6b153621cd5492213e53b47
Mikhail Subach <scorpibear@tut.by>
2016-07-26 21:11:37 +0300

    base serializer uses correct JSON format to make parsing strings with big nested objects possible

--- app/chessbase/base-serializer.js
+++ app/chessbase/base-serializer.js
@@ -1,9 +1,9 @@
 var toReadableString = function(baseObject) {
 	var evaluationToString = function (positionObject) {
 		if(positionObject.e){
-			var value = (typeof(positionObject.e.v) == "number") ? positionObject.e.v : "'" + positionObject.e.v + "'";
-			var evaluationPropertiesAsString = "v: " + value + ", d: " + positionObject.e.d;
-			return "e: {" + evaluationPropertiesAsString + "}, ";
+			var value = (typeof(positionObject.e.v) == "number") ? positionObject.e.v : '"' + positionObject.e.v + '"';
+			var evaluationPropertiesAsString = '"v": ' + value + ', "d": ' + positionObject.e.d;
+			return '"e": {' + evaluationPropertiesAsString + "}, ";
 		}
 		return "";
 	};
@@ -13,45 +13,22 @@ var toReadableString = function(baseObject) {
 			for(var i= 0; i<positionObject.s.length; i++) {
 				if(result!="") result = result + ",\n";
 				for(var j=0;j<level;j++) result += " ";
-				result += "{m: '" + positionObject.s[i].m + "', n: " + positionObject.s[i].n + ", c: '" +
-					positionObject.s[i].c + "', " + evaluationToString(positionObject.s[i]) +
-					"s: [" + subItemsToString(positionObject.s[i], level+1) + "]}";
+				result += '{"m": "' + positionObject.s[i].m + '", "n": ' + positionObject.s[i].n + ', "c": "' +
+					positionObject.s[i].c + '", ' + evaluationToString(positionObject.s[i]) +
+					'"s": [' + subItemsToString(positionObject.s[i], level+1) + "]}";
 			}
 		}
 		if(result!="") result = "\n" + result;
 		return result;
 	};
-	var result = "{m: '', n: 0, c: 'b', t: 'wb', " + evaluationToString(baseObject) + "s: [";
+	var result = '{"m": "", "n": 0, "c": "b", "t": "wb", ' + evaluationToString(baseObject) + '"s": [';
 	result += subItemsToString(baseObject,0);
 	result += "]}";
 	return result;
 };
 
 var toShortestString = function(baseObject) {
-	var evaluationToString = function (positionObject) {
-		if(positionObject.e){
-			var value = (typeof(positionObject.e.v) == "number") ? positionObject.e.v : "'" + positionObject.e.v + "'";
-			var evaluationPropertiesAsString = "v:" + value + ",d:" + positionObject.e.d;
-			return "e:{" + evaluationPropertiesAsString + "},";
-		}
-		return "";
-	};
-	var subItemsToString = function(positionObject) {
-		var result = "";
-		if(positionObject.s) {
-			for(var i= 0; i<positionObject.s.length; i++) {
-				if(result!="") result = result + ",";
-				result += "{m:'" + positionObject.s[i].m + "',n:" + positionObject.s[i].n + ",c:'" +
-					positionObject.s[i].c + "'," + evaluationToString(positionObject.s[i]) +
-					"s:[" + subItemsToString(positionObject.s[i]) + "]}";
-			}
-		}
-		return result;
-	};
-	var result = "{m:'',n:0,c:'b',t:'wb',s:[";
-	result += subItemsToString(baseObject);
-	result += "]}";
-	return result;
+	return JSON.stringify(baseObject);
 };
 
 module.exports.stringify = function(baseObject, readable){
@@ -65,7 +42,7 @@ module.exports.stringify = function(baseObject, readable){
 module.exports.parse = function(str) {
 	var base = {};
 	try {
-		base = (new Function("var base = " + str + "; return base;"))();
+		base = JSON.parse(str);
 	} catch (err) {
 		console.error(err + ". Could not parse base from: " + str)
 	}
--- spec/chessbase/base-serializer-spec.js
+++ spec/chessbase/base-serializer-spec.js
@@ -4,16 +4,22 @@ describe("baseSerializer", function () {
     describe("parse", function() {
         it("simple parsing", function() {
             expect(serializer.parse(
-                "{m: '', n: 0, c: 'b', t: 'wb', s: [" +
-                "{m: 'd4', n: 1, c: 'w', e: {v: 0.13, d: 30}, s: []}]}")).toEqual(
+                '{"m": "", "n": 0, "c": "b", "t": "wb", "s": [' +
+                '{"m": "d4", "n": 1, "c": "w", "e": {"v": 0.13, "d": 30}, "s": []}]}')).toEqual(
                  {m: '', n: 0, c: 'b', t: 'wb', s: [
                  {m: 'd4', n: 1, c: 'w', e: {v: 0.13, d: 30}, s: []}]});
         });
         it("simple stringify", function() {
-            expect(serializer.stringify({m: '', n: 0, c: 'b', t: 'wb', s: []}, true)).toEqual("{m: '', n: 0, c: 'b', t: 'wb', s: []}");
+            expect(serializer.stringify({m: '', n: 0, c: 'b', t: 'wb', s: []}, true)).toEqual('{"m": "", "n": 0, "c": "b", "t": "wb", "s": []}');
         })
 		it("saves evaluation for start position", function() {
-		    expect(serializer.stringify({m: '', n: 0, c: 'b', t: 'wb', e: {v: 0.12, d: 34}, s: []}, true)).toEqual("{m: '', n: 0, c: 'b', t: 'wb', e: {v: 0.12, d: 34}, s: []}")
+		    expect(serializer.stringify({m: '', n: 0, c: 'b', t: 'wb', e: {v: 0.12, d: 34}, s: []}, true))
+                .toEqual('{"m": "", "n": 0, "c": "b", "t": "wb", "e": {"v": 0.12, "d": 34}, "s": []}')
 		})
+        it("shortest string", function() {
+            let base = {m: '', n: 0, c: 'b', t: 'wb', s: [
+                 {m: 'd4', n: 1, c: 'w', e: {v: 0.13, d: 30}, s: []}]};
+            expect(serializer.stringify(base, false)).toEqual(JSON.stringify(base));
+        })
     })
 })
