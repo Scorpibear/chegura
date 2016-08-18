5322f2ee30f0165e874dc67fcfbfb3fd1c29175f
Mikhail Subach <scorpibear@tut.by>
2016-08-18 21:38:57 +0300

    Issue #16 fixed: depth is not increased if there is no continuation

--- app/analysis/depth-selector.js
+++ app/analysis/depth-selector.js
@@ -5,7 +5,8 @@ const baseIterator = require('../../app/chessbase/base-iterator');
 
 module.exports.getDepthToAnalyze = function(pathOfMoves, base) {
   let positionObject = baseIterator.findPositionObject(pathOfMoves, base);
-  if(positionObject && positionObject.hasOwnProperty('e') && positionObject.e.hasOwnProperty('d') && positionObject.e.d >= DEFAULT_DEPTH) {
+  if(positionObject && positionObject.hasOwnProperty('e') && positionObject.e.hasOwnProperty('d') &&
+    positionObject.e.d >= DEFAULT_DEPTH && positionObject.hasOwnProperty('s') && positionObject.s.length) {
     return positionObject.e.d + INCREMENT;
   }
   return DEFAULT_DEPTH;
--- spec/analysis/depth-selector-spec.js
+++ spec/analysis/depth-selector-spec.js
@@ -11,9 +11,19 @@ describe('depthSelector', function() {
     });
     it('returns depth+2 if  current depth is more than 32', function() {
       let path = ['e4','e6'];
-      let base = {m: '', s: [{m: 'e4', s: [{m: 'e6', e: {v: 0.1, d:33}}]}]};
+      let base = {m: '', s: [{m: 'e4', s: [{m: 'e6', e: {v: 0.1, d:33}, s: [{m: 'Nf3', e: {v: 0.12, d: 32}}]}]}]};
       expect(depthSelector.getDepthToAnalyze(path, base)).toEqual(35);
-    })
+    });
+    it('returns default depth if position has no continuation', function() {
+      let path = ['e4','e6'];
+      let base = {m: '', s: [{m: 'e4', s: [{m: 'e6', e: {v: 0.1, d:32}}]}]};
+      expect(depthSelector.getDepthToAnalyze(path, base)).toEqual(32);
+    });
+    it('returns default depth if position has no continuation even if current depth is high', function() {
+      let path = ['e4','e6'];
+      let base = {m: '', s: [{m: 'e4', s: [{m: 'e6', e: {v: 0.1, d:44}}]}]};
+      expect(depthSelector.getDepthToAnalyze(path, base)).toEqual(32);
+    });
   });
   describe('MAX_DEPTH', function() {
     it('300', function() {
