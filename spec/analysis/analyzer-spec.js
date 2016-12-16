32463d4d626be77e290027cb3ae6d05e0a95d8d3
Mikhail Subach <scorpibear@tut.by>
2016-12-16 14:29:49 +0300

    spec fixed

--- spec/analysis/analyzer-spec.js
+++ spec/analysis/analyzer-spec.js
@@ -1,68 +1,51 @@
 ﻿"use strict";
 
-describe('analyzer', function () {
-    var analyzer = require('../../app/analysis/analyzer');
-    describe('analyzeLater', function () {
-        it('do not analyze start position if first move is requested', function (done) {
-            analyzer.resetQueue();
-            //var baseManager = { addToBase: function () { } };
-            //analyzer.setBaseManager(baseManager);
-            analyzer.analyzeLater(['e4'], { m: '', s: [{ m: 'd4' }]}, 1);
-            var movesToAnalyze = analyzer.getQueue();
-            done();
-			expect(movesToAnalyze).toEqual([['e4']])
-        });
-        it('do not analyze first move if it is in base', function (done) {
-            analyzer.resetQueue();
-            //var baseManager = { addToBase: function () { } };
-            //analyzer.setBaseManager(baseManager);
-            analyzer.analyzeLater(['d4', 'e6'], { m: '', s: [{ m: 'd4' }] }, 1);
-            var movesToAnalyze = analyzer.getQueue();
-			expect(movesToAnalyze).toEqual([['d4', 'e6']]);
-			done()
-        });
-		it('re-analyze start position if position is already in base', function (done) {
-			analyzer.resetQueue();
-            //var baseManager = { addToBase: function () { } };
-            //analyzer.setBaseManager(baseManager);
-            analyzer.analyzeLater([], { m: '', s: [{ m: 'd4' }] }, 1);
-            var movesToAnalyze = analyzer.getQueue();
-            done();
-			expect(movesToAnalyze).toEqual([[]])
-		});
-		it('re-analyze first move if position is already in base', function (done) {
-			analyzer.resetQueue();
-            //var baseManager = { addToBase: function () { } };
-            //analyzer.setBaseManager(baseManager);
-            analyzer.analyzeLater(['d4'], { m: '', s: [{ m: 'd4' }] }, 1);
-            var movesToAnalyze = analyzer.getQueue();
-            done();
-			expect(movesToAnalyze).toEqual([['d4']])
-		});
-		it('re-analyze position if it is already in base', function (done) {
-			analyzer.resetQueue();
-            //var baseManager = { addToBase: function () { done() } };
-            //analyzer.setBaseManager(baseManager);
-            analyzer.analyzeLater(['d4', 'e5'], { m: '', s: [{ m: 'd4', s: [{m: 'e5'}] }] }, 1);
-            var movesToAnalyze = analyzer.getQueue();
-			expect(movesToAnalyze).toEqual([['d4', 'e5']]);
-			done();
-		});
+describe('analyzer', function() {
+  var analyzer = require('../../app/analysis/analyzer');
+  describe('analyzeLater', function() {
+    it('do not analyze start position if first move is requested', function(done) {
+      analyzer.resetQueue();
+      analyzer.analyzeLater(['e4'], {m: '', s: [{m: 'd4'}]}, 1);
+      var movesToAnalyze = analyzer.getQueue();
+      done();
+      expect(movesToAnalyze).toEqual([['e4']]);
     });
-    describe('setUciOptions', function() {
-      it('calls setUciOptions of analyzer-sync', function() {
-        let options = {};
-        let analyzerSync = require('../../app/analysis/analyzer-sync');
-        spyOn(analyzerSync, 'setUciOptions');
-
-        analyzer.setUciOptions(options);
-        
-        expect(analyzerSync.setUciOptions).toHaveBeenCalledWith(options);
-      })
-    })
-    /*describe('analyze', function() {
-        it('add main line evaluation if optimization queue is empty', function() {
-            expect(false).toBe(true);
-        })
-    })*/
+    it('do not analyze first move if it is in base', function(done) {
+      analyzer.resetQueue();
+      analyzer.analyzeLater(['d4', 'e6'], {m: '', s: [{m: 'd4'}]}, 1);
+      var movesToAnalyze = analyzer.getQueue();
+      expect(movesToAnalyze).toEqual([['d4', 'e6']]);
+      done();
+    });
+    it('re-analyze start position if position is already in base', function(done) {
+      analyzer.resetQueue();
+      analyzer.analyzeLater([], {m: '', s: [{m: 'd4'}]}, 1);
+      var movesToAnalyze = analyzer.getQueue();
+      done();
+      expect(movesToAnalyze).toEqual([[]]);
+    });
+    it('re-analyze first move if position is already in base', function(done) {
+      analyzer.resetQueue();
+      analyzer.analyzeLater(['d4'], {m: '', s: [{m: 'd4'}]}, 1);
+      var movesToAnalyze = analyzer.getQueue();
+      done();
+      expect(movesToAnalyze).toEqual([['d4']]);
+    });
+    it('re-analyze position if it is already in base', function(done) {
+      analyzer.resetQueue();
+      analyzer.analyzeLater(['d4', 'e5'], {m: '', s: [{m: 'd4', s: [{m: 'e5'}]}]}, 1);
+      var movesToAnalyze = analyzer.getQueue();
+      expect(movesToAnalyze).toEqual([['d4', 'e5']]);
+      done();
+    });
+  });
+  describe('setUciOptions', function() {
+    it('calls setUciOptions of analyzer-sync', function() {
+      let options = {};
+      let analyzerSync = require('../../app/analysis/analyzer-sync');
+      spyOn(analyzerSync, 'setUciOptions');
+      analyzer.setUciOptions(options);
+      expect(analyzerSync.setUciOptions).toHaveBeenCalledWith(options);
+    });
+  });
 });
