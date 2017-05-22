27ad2ddd4a5367cdc31b789df824a39ff5de14a4
Mikhail Subach <scorpibear@tut.by>
2017-05-22 12:46:40 +0300

    increment best answer depth if position depth > best answer depth > default depth

--- app/analysis/depth-selector.js
+++ app/analysis/depth-selector.js
@@ -7,9 +7,12 @@ const baseIterator = require('../../app/chessbase/base-iterator');
 
 module.exports.getDepthToAnalyze = function(pathOfMoves, base) {
   let positionObject = baseIterator.findPositionObject(pathOfMoves, base);
-  if(positionObject && positionObject.hasOwnProperty('e') && positionObject.e.hasOwnProperty('d') &&
-    positionObject.e.d >= DEFAULT_DEPTH && positionObject.hasOwnProperty('s') && positionObject.s.length) {
-    return positionObject.e.d + INCREMENT;
+  if (positionObject && positionObject.hasOwnProperty('e') && positionObject.e.hasOwnProperty('d') &&
+    (positionObject.e.d >= DEFAULT_DEPTH) && positionObject.hasOwnProperty('s') && positionObject.s.length) {
+    if (positionObject.s[0].e.d < DEFAULT_DEPTH - INCREMENT) {
+      return DEFAULT_DEPTH;
+    }
+    return (positionObject.s[0].e && positionObject.s[0].e.d && positionObject.e.d > positionObject.s[0].e.d) ? (positionObject.s[0].e.d + INCREMENT) : positionObject.e.d + INCREMENT;
   }
   return DEFAULT_DEPTH;
 };
