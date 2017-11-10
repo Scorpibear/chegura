9efc4766fa2f80b444e46733424db6a6e62a9f56
Mikhail Subach <scorpibear@tut.by>
2017-11-10 10:59:52 +0300

    test added for engine.analyzeToDepth

--- spec/analysis/engine-spec.js
+++ spec/analysis/engine-spec.js
@@ -2,9 +2,16 @@ describe('engine', function() {
   const engine = require('../../app/analysis/engine');
 
   describe('analyzeToDepth', () => {
+    it('calls run process', () => {
+      let UCI = require('uci');
+      spyOn(UCI.prototype, 'runProcess').and.returnValue(Promise.resolve(0));
 
+      engine.analyzeToDepth("", 5);
+
+      expect(UCI.prototype.runProcess).toHaveBeenCalled();
+    });
   });
   describe('setUciOptions', () => {
-    
+    it('sets the options for the next run');
   });
 });
