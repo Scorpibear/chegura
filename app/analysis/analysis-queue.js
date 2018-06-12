c5870410650bf94f6ae10cb6fff63a91b088b629
Mikhail Subach <scorpibear@tut.by>
2018-06-12 13:18:53 +0300

    queue.getFirst does not remove

--- app/analysis/analysis-queue.js
+++ app/analysis/analysis-queue.js
@@ -2,84 +2,79 @@
 // P1 - external requests for new positions
 // P2 - optimizations of not analyzed deep enough
 // P3 - main line clarification
-var emptyQueue = [[],[],[],[]]
+var emptyQueue = [[], [], [], []];
 var priorities = emptyQueue.length;
 
-var synchronizer = require('./synchronizer')
-var filename = 'analysis-queue.json'
+var synchronizer = require('./synchronizer');
+var filename = 'analysis-queue.json';
 
 // attach the .equals method to Array's prototype to call it on any array
-Array.prototype.equals = function (array) {
-    // if the other array is a falsy value, return
-    if (!array)
-        return false;
+Array.prototype.equals = function(array) {
+  // if the other array is a falsy value, return
+  if (!array)
+    return false;
 
-    // compare lengths - can save a lot of time
-    if (this.length != array.length)
-        return false;
+  // compare lengths - can save a lot of time
+  if (this.length !== array.length)
+    return false;
 
-    for (var i = 0, l=this.length; i < l; i++) {
-        // Check if we have nested arrays
-        if (this[i] instanceof Array && array[i] instanceof Array) {
-            // recurse into the nested arrays
-            if (!this[i].equals(array[i]))
-                return false;
-        }
-        else if (this[i] != array[i]) {
-            // Warning - two different object instances will never be equal: {x:20} != {x:20}
-            return false;
-        }
+  for (let i = 0, l = this.length; i < l; i++) {
+    // Check if we have nested arrays
+    if (this[i] instanceof Array && array[i] instanceof Array) {
+      // recurse into the nested arrays
+      if (!this[i].equals(array[i]))
+        return false;
+    } else if (this[i] !== array[i]) {
+      // Warning - two different object instances will never be equal: {x:20} != {x:20}
+      return false;
     }
-    return true;
-}
+  }
+  return true;
+};
 
 var queue = synchronizer.loadQueue(filename, emptyQueue)
 
 var save = function() {
-    synchronizer.saveQueue(filename, queue)
-}
+  synchronizer.saveQueue(filename, queue);
+};
 
-var needToSave = false;
-
-module.exports.getFirst = function () {
-    var item = null;
-    var priority = 0;
-    if(needToSave) save();
-    while(!item && priority < priorities) {
-        item = queue[priority++].shift();
-    }
-    needToSave = (item!=null);
-    return item;
-}
+module.exports.getFirst = () => {
+  let item = null;
+  let priority = 0;
+  while (!item && priority < priorities) {
+    item = queue[priority++][0];
+  }
+  return item;
+};
 
 module.exports.push = function(item, priority) {
-    if(!item) throw Error('Could not push to queue null item')
-    if(priority>priorities) throw Error('Priority ' + priority + ' exceeds max ' + priorities);
-    for(var q=0; q<priorities; q++) {
-        for(var i=0; i<queue[q].length; i++) {
-            if(queue[q][i].equals(item)) {
-                return;
-            }
-        }
+  if (!item) throw Error('Could not push to queue null item');
+  if (priority > priorities) throw Error('Priority ' + priority + ' exceeds max ' + priorities);
+  for (let q = 0; q < priorities; q++) {
+    for (let i = 0; i < queue[q].length; i++) {
+      if (queue[q][i].equals(item)) {
+        return;
+      }
     }
-    queue[priority].push(item)
-    console.log(item + ' is added to p' + priority + ' queue')
-    save()
-}
+  }
+  queue[priority].push(item);
+  console.log(item + ' is added to p' + priority + ' queue');
+  save();
+};
 
 module.exports.empty = function() {
-    for(var i=0; i<priorities; i++) {
-        queue[i] = []
-    }
-    save()
-}
+  for (let i = 0; i < priorities; i++) {
+    queue[i] = [];
+  }
+  save();
+};
 
 module.exports.getQueue = function() {
-    var result = [];
-    for(var q=0; q<priorities; q++) {
-        for(var i=0; i<queue[q].length; i++) {
-            result.push(queue[q][i]);
-        }
+  var result = [];
+  for (let q = 0; q < priorities; q++) {
+    for (let i = 0; i < queue[q].length; i++) {
+      result.push(queue[q][i]);
     }
-    return result;
-}
+  }
+  return result;
+};
--- spec/analysis/analysis-queue-spec.js
+++ spec/analysis/analysis-queue-spec.js
@@ -1,35 +1,42 @@
+const queue = require('../../app/analysis/analysis-queue');
+
 describe('analysisQueue', function() {
-    describe('push', function() {
-        it('calls synchronizer.saveQueue', function() {
-            var queue = require('../../app/analysis/analysis-queue')
-            var synchronizer = require('../../app/analysis/synchronizer')
-            spyOn(synchronizer,'saveQueue')
-            queue.push([],0)
-            expect(synchronizer.saveQueue).toHaveBeenCalled()
-        });
-        it('does not add to queue what is already there', () => {
-            var queue = require('../../app/analysis/analysis-queue')
-            queue.empty();                        
-
-            queue.push(['d4'], 2);
-            queue.push(['d4'], 3);
-
-            expect(queue.getQueue()).toEqual([['d4']]);
-        })
+  beforeEach(() => {
+    queue.empty();
+  });
+  describe('delete', () => {
+    it('deletes item');
+    it('calls save');
+  });
+  describe('push', function() {
+    it('calls synchronizer.saveQueue', function() {
+      var synchronizer = require('../../app/analysis/synchronizer');
+      spyOn(synchronizer, 'saveQueue');
+      queue.push([], 0);
+      expect(synchronizer.saveQueue).toHaveBeenCalled();
     });
-    describe('getFirst', function() {
-        it("returns first from queue", function() {
-            var queue = require('../../app/analysis/analysis-queue')
-            queue.empty();
-            queue.push(["d4"],1);
-            expect(queue.getFirst()).toEqual(["d4"])
-        })
+    it('does not add to queue what is already there', () => {
+      queue.push(['d4'], 2);
+      queue.push(['d4'], 3);
+
+      expect(queue.getQueue()).toEqual([['d4']]);
     });
-    describe('empty', function() {
-        it('empties the all queue', () => {
-            var queue = require('../../app/analysis/analysis-queue')
-            queue.empty();
-            expect(queue.getQueue()).toEqual([]);
-        })
-    })
-})
+  });
+  describe('getFirst', function() {
+    it("returns first from queue", function() {
+      queue.push(["d4"], 1);
+      expect(queue.getFirst()).toEqual(["d4"]);
+    });
+    it('does not modify queue', () => {
+      queue.push(['d4'], 1);
+      queue.getFirst();
+      expect(queue.getQueue()).toEqual([['d4']]);
+    });
+  });
+  describe('empty', function() {
+    it('empties the all queue', () => {
+      queue.empty();
+      expect(queue.getQueue()).toEqual([]);
+    });
+  });
+});
