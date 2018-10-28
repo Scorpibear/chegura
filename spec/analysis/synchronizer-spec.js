1b0bf8fb988d73b63c6acb069b0384bb83bb77b7
Mikhail Subach <scorpibear@tut.by>
2018-10-28 20:29:41 +0300

    save queue synchronously

--- spec/analysis/synchronizer-spec.js
+++ spec/analysis/synchronizer-spec.js
@@ -1,17 +1,43 @@
+const fs = require('fs');
+const queueSerializer = require('../../app/analysis/queue-serializer');
+
 describe('synchronizer', function() {
-    var synchronizer = require('../../app/analysis/synchronizer')
-    describe('loadQueue', function () {
-        it('returns default queue if file not found', function() {
-            var queue = [['h3']];
-            expect(synchronizer.loadQueue('not-existed-file.json', queue)).toBe(queue)
-        })
-    })
-    describe('saveQueue', function() {
-        it('calls queueSerializer.stringify', function() {
-            var queueSerializer = require('../../app/analysis/queue-serializer')
-            spyOn(queueSerializer,'stringify')
-            synchronizer.saveQueue('test-filename.tmp',[[],[],[],[]])
-            expect(queueSerializer.stringify).toHaveBeenCalled()
-        })
-    })
-})
+  var synchronizer = require('../../app/analysis/synchronizer');
+  describe('loadQueue', function () {
+    var queue = [['h3']];
+    it('returns default queue if file not found', function() {
+      expect(synchronizer.loadQueue('not-existed-file.json', queue)).toBe(queue);
+    });
+    it('logs error if could not load', () => {
+      spyOn(console, 'error').and.stub();
+      spyOn(fs, 'readFileSync').and.throwError('bad things');
+      synchronizer.loadQueue('file.json', queue);
+      expect(console.error).toHaveBeenCalled();
+    });
+    it('use queueSerializer.parse', () => {
+      spyOn(fs, 'readFileSync').and.returnValue('queue content');
+      spyOn(queueSerializer, 'parse').and.returnValue([['e5']]);
+      expect(synchronizer.loadQueue('file.json', queue)).toEqual([['e5']]);
+    });
+  });
+  describe('saveQueue', function() {
+    const queueContent = [[],[],[],[]];
+    it('calls queueSerializer.stringify', function() {
+      var queueSerializer = require('../../app/analysis/queue-serializer');
+      spyOn(queueSerializer,'stringify');
+      synchronizer.saveQueue('test-filename.tmp', queueContent);
+      expect(queueSerializer.stringify).toHaveBeenCalled();
+    });
+    it('call writeFileSync to avoid issues with multiple writeFile', () => {
+      spyOn(fs, 'writeFileSync').and.stub();
+      synchronizer.saveQueue('file.name', queueContent);
+      expect(fs.writeFileSync).toHaveBeenCalled();
+    });
+    it('logs error if writeFileSync failed', () => {
+      spyOn(console, 'error').and.stub();
+      spyOn(fs, 'writeFileSync').and.throwError('bad things');
+      synchronizer.saveQueue('file.name', queueContent);
+      expect(console.error).toHaveBeenCalled();
+    });
+  });
+});
