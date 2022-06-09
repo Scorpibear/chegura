9df2b4c939fffd36998b60ee13c4556a95bb7a6b
Mikhail Subach <mikhail_subach@epam.com>
2022-06-09 14:59:30 +0200

    fix handling of incorrect POST /analyze body

--- app/request-processor.js
+++ app/request-processor.js
@@ -1,39 +1,41 @@
-var analysisPriority = require('./analysis/analysis-priority');
+var analysisPriority = require("./analysis/analysis-priority");
 
-class RequestProcessor{
-  constructor({baseManager, queueProcessor, usageStatistics, analyzer}){
+class RequestProcessor {
+  constructor({ baseManager, queueProcessor, usageStatistics, analyzer }) {
     this.analyzer = analyzer;
     this.baseManager = baseManager;
     this.queueProcessor = queueProcessor;
     this.usageStatistics = usageStatistics;
   }
   getBase(req, res) {
-    let userId = new URL(req.url, 'https://hostname').searchParams.get('userid');
+    let userId = new URL(req.url, "https://hostname").searchParams.get(
+      "userid"
+    );
     res.writeHead(200, {
-      'Content-Type': 'text/plain',
-      'Access-Control-Allow-Origin': '*'
+      "Content-Type": "text/plain",
+      "Access-Control-Allow-Origin": "*",
     });
     res.end(this.baseManager.getBaseAsString());
     this.usageStatistics.registerBaseRequest(userId);
   }
   getFenBase(req, res) {
     res.writeHead(200, {
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*'
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
     });
     res.end(JSON.stringify(this.baseManager.getFenBase()));
   }
   getFenData(req, res) {
-    let url = new URL(req.url, 'https://hostname');
-    let fen = url.searchParams.get('fen');
-    if(fen) {
+    let url = new URL(req.url, "https://hostname");
+    let fen = url.searchParams.get("fen");
+    if (fen) {
       fen = decodeURI(fen);
       console.log(`requesting fenData for '${fen}'`);
       res.writeHead(200, {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*'
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
       });
-      res.end(JSON.stringify(this.baseManager.getFen({fen})));
+      res.end(JSON.stringify(this.baseManager.getFen({ fen })));
     } else {
       res.writeHead(422);
       res.end();
@@ -41,46 +43,57 @@ class RequestProcessor{
   }
   default(res) {
     res.writeHead(200, {
-      'Content-Type': 'text/plain',
-      'Access-Control-Allow-Origin': '*'
+      "Content-Type": "text/plain",
+      "Access-Control-Allow-Origin": "*",
     });
     res.end();
   }
   analyze(req, res) {
-    if (req.method === 'OPTIONS') {
+    if (req.method === "OPTIONS") {
       res.writeHead(200, {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Headers': 'Origin, Content-Type'
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Origin, Content-Type",
       });
-      res.end('');
+      res.end("");
     }
-    if (req.method === 'POST') {
-      req.on('data', chunk => {
-        var data = JSON.parse(chunk);
-        if (data.moves) {
-          this.analyzer.analyzeLater(data.moves, this.baseManager.getBase(),
-            analysisPriority.ExternalRequestsForNewPositions).then(() => {
-            return this.queueProcessor.process();
-          }).catch(err => console.error(err));
+    if (req.method === "POST") {
+      req.on("data", (chunk) => {
+        let data;
+        try {
+          data = JSON.parse(chunk);
+        } catch {}
+        if (data && data.moves) {
+          this.analyzer
+            .analyzeLater(
+              data.moves,
+              this.baseManager.getBase(),
+              analysisPriority.ExternalRequestsForNewPositions
+            )
+            .then(() => {
+              return this.queueProcessor.process();
+            })
+            .catch((err) => console.error(err));
         } else {
-          console.error('Incorrect data received:', data);
+          console.error(
+            `POST analyze: incorrect body received: '${chunk}'. It should be {moves: []} object`
+          );
         }
       });
-      res.writeHead(200, {'Access-Control-Allow-Origin': '*'});
-      res.end('');
+      res.writeHead(200, { "Access-Control-Allow-Origin": "*" });
+      res.end("");
     }
   }
   getUsersCount(req, res) {
     res.writeHead(200, {
-      'Content-Type': 'text/plain',
-      'Access-Control-Allow-Origin': '*'
+      "Content-Type": "text/plain",
+      "Access-Control-Allow-Origin": "*",
     });
     var usersCount = this.usageStatistics.getUsersCount();
     res.end(String(usersCount));
   }
   ping(req, res) {
-    res.writeHead(200, {'Access-Control-Allow-Origin': '*'});
-    res.end('');
+    res.writeHead(200, { "Access-Control-Allow-Origin": "*" });
+    res.end("");
     this.queueProcessor.process();
   }
 }
--- spec/request-processor-spec.js
+++ spec/request-processor-spec.js
@@ -1,98 +1,136 @@
-const RequestProcessor = require('../app/request-processor');
+const RequestProcessor = require("../app/request-processor");
 
-describe('requestProcessor', () => {
-  const stub = () => { /* just an empty stub for tests to later spyOn and mock */ };
-  const analyzer = {analyzeLater: () => Promise.resolve()};
-  const baseManager = {getBaseAsString: stub, getBase: stub, getFenBase: stub, getFen: stub};
-  const res = {writeHead: stub, end: stub, json: stub};
-  const moves = ['d4', 'Nf6'];
-  const queueProcessor = {process: stub};
-  const usageStatistics = {getUsersCount: stub, registerBaseRequest: stub};
-  const requestProcessor = new RequestProcessor({baseManager, queueProcessor, usageStatistics, analyzer});
+describe("requestProcessor", () => {
+  const stub = () => {
+    /* just an empty stub for tests to later spyOn and mock */
+  };
+  const analyzer = { analyzeLater: () => Promise.resolve() };
+  const baseManager = {
+    getBaseAsString: stub,
+    getBase: stub,
+    getFenBase: stub,
+    getFen: stub,
+  };
+  const res = { writeHead: stub, end: stub, json: stub };
+  const moves = ["d4", "Nf6"];
+  const queueProcessor = { process: stub };
+  const usageStatistics = { getUsersCount: stub, registerBaseRequest: stub };
+  const requestProcessor = new RequestProcessor({
+    baseManager,
+    queueProcessor,
+    usageStatistics,
+    analyzer,
+  });
 
-  describe('analyze', () => {
-    it('send moves to analyzer', () => {
-      const req = {method: 'POST', on: (event, handler) => handler(JSON.stringify({moves}))};
-      spyOn(analyzer, 'analyzeLater').and.returnValue(Promise.resolve());
+  describe("analyze", () => {
+    it("send moves to analyzer", () => {
+      const req = {
+        method: "POST",
+        on: (event, handler) => handler(JSON.stringify({ moves })),
+      };
+      spyOn(analyzer, "analyzeLater").and.returnValue(Promise.resolve());
       requestProcessor.analyze(req, res);
-      expect(analyzer.analyzeLater).toHaveBeenCalledWith(moves, baseManager.getBase(), 1);
+      expect(analyzer.analyzeLater).toHaveBeenCalledWith(
+        moves,
+        baseManager.getBase(),
+        1
+      );
     });
-    it('writes header on OPTIONS method', () => {
-      const req = { method: 'OPTIONS' };
-      spyOn(res, 'writeHead').and.stub();
+    it("writes header on OPTIONS method", () => {
+      const req = { method: "OPTIONS" };
+      spyOn(res, "writeHead").and.stub();
       requestProcessor.analyze(req, res);
       expect(res.writeHead).toHaveBeenCalled();
     });
-    it('logs error if moves where not posted', () => {
-      const req = { method: 'POST', on: (event, handler) => handler(JSON.stringify({})) };
-      spyOn(console, 'error').and.stub();
+    it("logs error if moves where not posted", () => {
+      const req = {
+        method: "POST",
+        on: (event, handler) => handler(JSON.stringify({})),
+      };
+      spyOn(console, "error").and.stub();
       requestProcessor.analyze(req, res);
       expect(console.error).toHaveBeenCalled();
     });
-    it('queue is not processed if analyzeLater fails', () => {
-      const req = {method: 'POST', on: (event, handler) => handler(JSON.stringify({moves}))};
-      spyOn(analyzer, 'analyzeLater').and.returnValue(Promise.reject());
-      spyOn(queueProcessor, 'process').and.stub();
+    it("queue is not processed if analyzeLater fails", () => {
+      const req = {
+        method: "POST",
+        on: (event, handler) => handler(JSON.stringify({ moves })),
+      };
+      spyOn(analyzer, "analyzeLater").and.returnValue(Promise.reject());
+      spyOn(queueProcessor, "process").and.stub();
       requestProcessor.analyze(req, res);
       expect(queueProcessor.process).not.toHaveBeenCalled();
-
+    });
+    it("log error and do not crash when incorrect body received", () => {
+      const req = {
+        method: "POST",
+        on: (event, handler) => handler("random body"),
+      };
+      spyOn(console, "error").and.stub();
+      requestProcessor.analyze(req, res);
+      expect(console.error).toHaveBeenCalledWith(
+        "POST analyze: incorrect body received: 'random body'. It should be {moves: []} object"
+      );
     });
   });
-  describe('default', () => {
-    it('sends response immediately', () => {
-      spyOn(res, 'end').and.stub();
+  describe("default", () => {
+    it("sends response immediately", () => {
+      spyOn(res, "end").and.stub();
       requestProcessor.default(res);
       expect(res.end).toHaveBeenCalled();
     });
   });
-  describe('getBase', () => {
-    it('save user info', () => {
-      const req = {url: ''};
-      spyOn(usageStatistics, 'registerBaseRequest');
+  describe("getBase", () => {
+    it("save user info", () => {
+      const req = { url: "" };
+      spyOn(usageStatistics, "registerBaseRequest");
       requestProcessor.getBase(req, res);
       expect(usageStatistics.registerBaseRequest).toHaveBeenCalled();
     });
   });
-  describe('getFenBase', () => {
-    it('provide fenbase as is', () => {
+  describe("getFenBase", () => {
+    it("provide fenbase as is", () => {
       const req = {};
-      spyOn(baseManager, 'getFenBase').and.stub();
+      spyOn(baseManager, "getFenBase").and.stub();
       requestProcessor.getFenBase(req, res);
       expect(baseManager.getFenBase).toHaveBeenCalled();
     });
   });
-  describe('getFenData', () => {
-    it('use info from baseManager', () => {
-      const fenData = {bestMove: 'Nf6', sp: 30, depth: 50};
-      spyOn(baseManager, 'getFen').and.returnValue(fenData);
-      spyOn(res, 'end');
-      requestProcessor.getFenData({url: '/api/fenData?fen=some valid FEN'}, res);
+  describe("getFenData", () => {
+    it("use info from baseManager", () => {
+      const fenData = { bestMove: "Nf6", sp: 30, depth: 50 };
+      spyOn(baseManager, "getFen").and.returnValue(fenData);
+      spyOn(res, "end");
+      requestProcessor.getFenData(
+        { url: "/api/fenData?fen=some valid FEN" },
+        res
+      );
       expect(res.end).toHaveBeenCalledWith(JSON.stringify(fenData));
     });
-    it('sends data to getFen as an object', () => {
-      const fen = 'let us imagine it is a correct FEN';
-      spyOn(baseManager, 'getFen');
-      requestProcessor.getFenData({url: `/api/fenData?fen=${fen}`}, res);
-      expect(baseManager.getFen).toHaveBeenCalledWith({fen});
+    it("sends data to getFen as an object", () => {
+      const fen = "let us imagine it is a correct FEN";
+      spyOn(baseManager, "getFen");
+      requestProcessor.getFenData({ url: `/api/fenData?fen=${fen}` }, res);
+      expect(baseManager.getFen).toHaveBeenCalledWith({ fen });
     });
-    it('sends 422 if fen is not specified', () => {
-      spyOn(res, 'writeHead');
-      requestProcessor.getFenData({url: '/api/fenData'}, res);
+    it("sends 422 if fen is not specified", () => {
+      spyOn(res, "writeHead");
+      requestProcessor.getFenData({ url: "/api/fenData" }, res);
       expect(res.writeHead).toHaveBeenCalledWith(422);
     });
   });
-  describe('getUsersCount', () => {
-    it('returns usageStatistics.getUsersCount', () => {
+  describe("getUsersCount", () => {
+    it("returns usageStatistics.getUsersCount", () => {
       const req = {};
-      spyOn(res, 'end');
-      spyOn(usageStatistics, 'getUsersCount').and.returnValue(42);
+      spyOn(res, "end");
+      spyOn(usageStatistics, "getUsersCount").and.returnValue(42);
       requestProcessor.getUsersCount(req, res);
-      expect(res.end).toHaveBeenCalledWith('42');
+      expect(res.end).toHaveBeenCalledWith("42");
     });
   });
-  describe('ping', () => {
-    it('ask queue processor to process queue', () => {
-      spyOn(queueProcessor, 'process').and.stub();
+  describe("ping", () => {
+    it("ask queue processor to process queue", () => {
+      spyOn(queueProcessor, "process").and.stub();
       requestProcessor.ping({}, res);
       expect(queueProcessor.process).toHaveBeenCalled();
     });
